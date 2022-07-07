7e701497fa2ad89679a3b1475e7ac51368022c62
Sandrine <sandrine.thominet@gmail.com>
2022-07-07 11:40:36 -0400

    Work in progress

--- src/components/breed-image/BreedImage.js
+++ src/components/breed-image/BreedImage.js
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import './breed-image.css'
-
-
+import React, { useState, useEffect } from 'react';
+import './breed-image.css';
 
 function BreedImage () {
     const [breed, selectedBreed] = useState('');
-    
+
     useEffect(() => {
         fetchImage();
-    }, []);
-    
+        }, []);
 
-    
     const fetchImage = async () => {
-        const res = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
-        const data = await res.json()
+        const res = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        const data = await res.json();
         const imageUrl = data.message
         selectedBreed(imageUrl);
-        }
+        };
 
     return (
       <div className="image-container">
-        <img className="image-card" src={{imageUrl}} />
+        <img className="image-card" src={breed} alt="doggopicture" />
       </div>
     );
   
